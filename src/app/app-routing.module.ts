3edfc21c1640f095d1d7fc852079c16d02e627e2
joshua-roberts <joshua.roberts@nist.gov>
2018-05-24 11:55:55 -0400

    changed default route from nodes to dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import {AssociationsComponent} from './associations/associations.component';
 import {ProhibitionsComponent} from './prohibitions/prohibitions.component';
 import {NLPMComponent} from './nlpm/nlpm.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import {UsersComponent} from './users/users.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/nodes', pathMatch: 'full' },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'users', component: UsersComponent },
   { path: 'associations', component: AssociationsComponent },
   { path: 'prohibitions', component: ProhibitionsComponent },
   { path: 'nlpm', component: NLPMComponent }
